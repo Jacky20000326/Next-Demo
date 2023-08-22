b010980fd06312810385bb2e9d1a36a1ffaa40fe
jacky_hong <Jj0932175456@gmail.com>
2023-08-22 22:41:29 +0800

    0822

--- app/_components/_content/ContentList/ArticleList.tsx
+++ app/_components/_content/ContentList/ArticleList.tsx
@@ -1,7 +1,38 @@
 import React from "react";
-
+import Styled from "./ArticleList.module.sass";
+import Image from "next/image";
 const ArticleList = () => {
-    return <div>ArticleList</div>;
+    let testArr = [1, 2, 3, 4, 5];
+    return (
+        <div className={Styled.ArticleListContainer}>
+            {testArr.map((item) => (
+                <div className={Styled.ArticleListBlock}>
+                    <div className={Styled.ArticleImgCube}>
+                        <Image
+                            width={120}
+                            height={120}
+                            alt="NextJS"
+                            src="/rust2.jpeg"
+                            style={{ objectFit: "contain" }}
+                        />
+                    </div>
+                    <div className={Styled.ArticleContentCube}>
+                        <div className={Styled.ArticleContentTitle}>
+                            基於 Nginx 實現一個灰度上線系統
+                        </div>
+                        <div className={Styled.ArticleContentTime}>
+                            2023年07月14日
+                        </div>
+                        <div className={Styled.ArticleContentIntroduction}>
+                            軟件開發一般不會上來就是最終版本，而是會一個版本一個版本的迭代。
+                            新版本上線前都會經過測試，但就算這樣，也不能保證上線了不出問題。
+                            所以，在公司裏上線新版本代碼一般都是通過灰度系…
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default ArticleList;
--- app/_components/_content/ContentList/listMenu.tsx
+++ app/_components/_content/ContentList/listMenu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Styled from "./listMenu.module.sass";
 import MenuFilter from "./menuFilter";
+import ArticleList from "./ArticleList";
 const List = () => {
     return (
         <div className={Styled.menuListContainer}>
             <MenuFilter />
+            <ArticleList />
         </div>
     );
 };
